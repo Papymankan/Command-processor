21bd7fab273237d8b0d9376a839ce57e4047b1a7
Papymankan <parsa84rostami@gmail.com>
2025-04-01 10:24:38 +0330

    handle create command errors

--- Test.java
+++ Test.java
@@ -4,15 +4,23 @@ import java.util.*;
 
 public class Test {
 
+	public static void WarnningMessage(String input) {
+		System.out.println("WARNNING ==> " + input);
+	}
+
+	public static void ErrorMessage(String input) {
+		System.out.println("ERROR ==> " + input);
+	}
+
 	public static boolean isValidFormat(String input) {
-		String regex = "^(\\S+)\\s(\\S+)(\\s\\(.*?\\))?\\s*\\{.*\\}$";
+		String regex = "^(\\S+)\\s(\\S+)(?:\\s*\\(\\s*[^()]+\\s*\\))?(?:\\s*\\{.*\\})?$";
 		return input.matches(regex);
 	}
 
-	public static void parseCommand(String input) {
+	public static void parseCommand(String input, Map<String, Map<String, List<String>>> myTypes) {
 		String CommandType = "";
 		String Type = "";
-		String Parameter = null;
+		String Parameter = "";
 		String JSONInput = "";
 
 		input = input.trim().replaceAll("\\s+", " ").toLowerCase();
@@ -20,10 +28,16 @@ public class Test {
 		if (isValidFormat(input)) {
 			CommandType = input.substring(0, input.indexOf(" "));
 			input = input.substring(input.indexOf(" ") + 1);
-			Type = input.substring(0, input.indexOf(" "));
-			input = input.substring(input.indexOf(" ") + 1);
 
-			if (input.charAt(0) == '(') {
+			if (input.indexOf(" ") != -1) {
+				Type = input.substring(0, input.indexOf(" "));
+				input = input.substring(input.indexOf(" ") + 1);
+			} else {
+				Type = input.substring(0, input.length());
+				input = "";
+			}
+
+			if (input.length() > 0 && input.charAt(0) == '(') {
 				Parameter = input.substring(0, input.indexOf(")") + 1);
 				input = input.substring(input.indexOf(")") + 2);
 			}
@@ -37,6 +51,17 @@ public class Test {
 			switch (CommandType) {
 				case "create":
 
+					if (!Parameter.equals("")) {
+						ErrorMessage("Create Command does not accept any parameters !");
+						break;
+					}
+
+					if (JSONInput.equals("")) {
+						ErrorMessage("Create Command accepts a JSON Input !");
+						break;
+					}
+
+					createType(Type, JSONInput, myTypes);
 					break;
 				case "insert":
 
@@ -58,8 +83,14 @@ public class Test {
 
 	}
 
-	public static void createType(String Type , String JSONInput) {
-		
+	public static void createType(String Type, String JSONInput, Map<String, Map<String, List<String>>> myTypes) {
+
+		if (myTypes.containsKey(Type)) {
+			ErrorMessage("The Type { " + Type + " } already exists");
+		} else {
+			System.out.println(Type + JSONInput);
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -75,7 +106,7 @@ public class Test {
 			if (input.equals("exit"))
 				break;
 
-			parseCommand(input);
+			parseCommand(input, myTypes);
 		}
 
 	}
